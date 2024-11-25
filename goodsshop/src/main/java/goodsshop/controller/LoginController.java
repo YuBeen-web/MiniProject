384b06d4114df5b0f029c082cae56cc4fd823220
301-18 <301-18@172.16.99.70>
2024-11-25 17:56:38 +0900

    미니프로젝트 2일차

--- goodsshop/src/main/java/goodsshop/controller/LoginController.java
+++ goodsshop/src/main/java/goodsshop/controller/LoginController.java
@@ -1,8 +1,21 @@
 package goodsshop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import goodsshop.service.login.IdcheckService;
 
 @Controller
+@RequestMapping("login")
 public class LoginController {
+	@Autowired
+	IdcheckService idcheckService;
 	
+	@PostMapping("userIdCheck")
+	public @ResponseBody int userIdCheck(String userId) {
+		return idcheckService.execute(userId);
+	}
 }
